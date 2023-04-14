8ed9f888f8f9c917f23c3877356e0afad2ca8ff7
Yann Mölle <hxcde@noreply.localhost>
2023-04-14 20:09:41 +0200

    „deb-flared-update.sh“ ändern

--- deb-flared-update.sh
+++ deb-flared-update.sh
@@ -4,6 +4,7 @@ touch flaredupdate.sh
 echo "curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb" | tee -a flaredupdate.sh
 echo "sudo dpkg -i cloudflared.deb" | tee -a flaredupdate.sh
 echo "rm cloudflared.deb" | tee -a flaredupdate.sh
+echo "systemctl restart cloudflared" | tee -a flaredupdate.sh
 crontab -l > mycron
 echo "0 0 * * * sh /opt/flaredupdate.sh >/dev/null 2>&1" >> mycron
 crontab mycron
